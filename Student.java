fef357d4663c1cbc5f761f86305d78722094e38f
ankurpapneja4 <ankurpapneja4@gmail.com>
2022-03-04 11:13:30 +0530

    Added Getter Setter For Email

--- Student.java
+++ Student.java
@@ -3,4 +3,12 @@ public class Student {
     private String lastName;
     private String email;
     private String marks;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
